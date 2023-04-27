cd0942c35358eb7e74cb992f2d991077e8ec49b4
James <veloxexcidium@outlook.com>
2023-04-27 13:09:43 +1000

    Esquared

--- A pair (even more unreadable version).py
+++ A pair (even more unreadable version).py
@@ -1,15 +1,15 @@
 #This is as unreadable as I could make it without using semicolons
-inp,item,itemPlural=5,'item','items'
-s,d,r=inp,item,itemPlural
+inp,item,itemPlural=5,'item','items' #Config
+s,d,r,q,l,x,u=inp,item,itemPlural,range,len,zip,None
 f=list(True if n=='1'else False for n in bin(s)[2:])
 o=[]
-for v,i in zip(f,range(len(f))):
- o.append(len(f)-i-1)if v else None
-for n,i in zip(o,range(len(o))):
- p=f'one {d}'if i==len(o)-1and s%2!=0 else'a '
- for z in range(o[i]):
+for v,i in x(f,q(l(f))):
+ o.append(l(f)-i-1)if v else u
+for n,i in x(o,q(l(o))):
+ p=f'one {d}'if i==l(o)-1and s%2!=0 else'a '
+ for z in q(o[i]):
   p+='pair'if z<1else'pairs'
   p+=' of 'if z!=o[i]-1else''
- p+=f' of {r}'if i==len(o)-2or i==len(o)-1and s%2==0else''
+ p+=f' of {r}'if i==l(o)-2or i==l(o)-1and s%2==0else''
  o[i]=p
 print(' plus '.join(o))
